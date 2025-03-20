855ba481d0bc3edf339851a6e8d98e0647183829
Maike2961 <mayki.silva@hotmail.com>
2025-03-20 18:48:17 -0300

    second commit

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import {TopwidgetComponent} from '../topwidget/topwidget.component'
-import { widgetContainers } from './widgetContainer';
-import { ChartsComponent } from "../charts/charts.component";
+import { widgetContainers } from '../topwidget/widgetContainer';
+import { ChartsComponent } from "../gastosMensais/charts.component";
 import { ChartPizzaComponent } from "../chart-pizza/chart-pizza.component";
+import { TabalaCaloteComponent } from "../devedores/tabala-calote.component";
 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [TopwidgetComponent, ChartsComponent, ChartPizzaComponent],
+  imports: [TopwidgetComponent, ChartsComponent, ChartPizzaComponent, TabalaCaloteComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
