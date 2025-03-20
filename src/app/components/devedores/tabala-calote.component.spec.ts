b1a384af96be30e0968260944d0089c311fef358
Maike2961 <mayki.silva@hotmail.com>
2025-03-20 18:24:39 -0300

    feat: add grafico para o que esta devendo

---
+++ src/app/components/devedores/tabala-calote.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TabalaCaloteComponent } from './tabala-calote.component';
+
+describe('TabalaCaloteComponent', () => {
+  let component: TabalaCaloteComponent;
+  let fixture: ComponentFixture<TabalaCaloteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TabalaCaloteComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TabalaCaloteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/devedores/tabala-calote.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FinanceServiceService } from '../../service/finance-service.service';
+import { CommonModule } from '@angular/common';
+import { Financias } from '../../interface/interface';
+
+@Component({
+  selector: 'app-tabala-calote',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tabala-calote.component.html',
+  styleUrl: './tabala-calote.component.scss'
+})
+export class TabalaCaloteComponent implements OnInit{
+  
+  constructor(private service: FinanceServiceService){}
+
+  tabelaCaloteiro: any[] = [];
+
+  ngOnInit(): void {
+    this.service.getAllFinancias().subscribe(data => {
+        const naoPagos = this.YesOrNot(data)
+
+        this.tabelaCaloteiro = naoPagos.map(item => ({
+          categoria: item.categoria,
+          descricao: item.descricao,
+          preco: Number(item.preco)
+        }));
+    });
+  }
+
+
+  private YesOrNot(data: Financias[]){
+    return data.filter(item => item.isPago == "não")
+  }
+
+}
