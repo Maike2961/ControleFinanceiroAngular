faad92053d15a6c0437e153c3a5944c1e66f809b
Maike2961 <mayki.silva@hotmail.com>
2025-05-19 19:02:38 -0300

    delete: old front and backend

--- backend/demo/src/main/java/io/github/controlefinanceiro/demo/common/dto/ErroCampo.java
+++
@@ -1,4 +0,0 @@
-package io.github.controlefinanceiro.demo.common.dto;
-
-public record ErroCampo(String campo, String erro) {
-}
--- frontend/project/src/app/pipes/date-formt.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DateFormtPipe } from './date-formt.pipe';
-
-describe('DateFormtPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateFormtPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
